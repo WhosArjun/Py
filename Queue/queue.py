3ed3e81cdad0be12791f62992d85dcf10fd4f88c
WhosArjun <arjun.arunkumar.v@gmail.com>
2026-07-13 19:36:04 -0400

    queue

---
+++ Queue/queue.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+        self.size = 0
+    
+    def __len__(self):
+        return self.size
+    
+    def __repr__(self):
+        pass 
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.rear is None:
+            self.front = self.rear = new_node 
+        else:
+            self.rear.next = new_node
+            self.rear = new_node 
+
+        self.size += 1
+     
+
+    def dequeue(self, value):
+        if self.front is None:
+            raise IndexError("Index out of bounds")
+        dequeue_value = self.front.value
+        self.front = self.front.next 
+
+        if self.front is None:
+            self.rear = None 
+        
+        self.size -= 1
+
+    def peek(self):
+        if self.front is None:
+            raise IndexError("Queue is empty")
+        return self.front.value
+
+    def is_empty(self):
+        return self.front is None 
