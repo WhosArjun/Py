eaeea0a5b46e28527155c86d355a354567ed1dd6
WhosArjun <arjun.arunkumar.v@gmail.com>
2026-07-13 20:45:54 -0400

    hash

---
+++ PythonDS/Hashmap/hash_map.py
@@ -0,0 +1,34 @@
+class HashMap:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.size = 0
+        self.buckets = {[] for _ in range(capacity)}
+
+    def __len__(self):
+        pass 
+
+    def __contains_(self, key):
+        pass 
+
+    def put(self, key, value):
+        pass 
+
+    def get(self, key):
+        pass 
+
+    def remove(self, key):
+        pass 
+
+    def keys(self):
+        pass 
+
+    def values(self):
+        pass 
+
+    def items(self):
+        pass 
+
+    def _hash_function(self, key):
+        pass 
+     
+
